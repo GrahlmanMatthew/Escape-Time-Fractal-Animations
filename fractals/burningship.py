03acdf6dee58c79eb9f7f88eab1d79abf3405407
Matthew E. Grahlman <grahlmanmatthew@gmail.com>
2021-10-29 11:53:39 -0400

    Refactored Burning Ship to use inheritence

--- fractals/burningship.py
+++ fractals/burningship.py
@@ -2,12 +2,14 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from fractals.mandelbrot import Mandelbrot
 
-GIF_OUTPUT_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output', 'burningship.gif'))
+GIF_OUTPUT_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output', 'burningship2.gif'))
 
-class BurningShip:  
-    def __init__(self, startX= -2, startY= -1.5, WIDTH= 3, HEIGHT= 3, DPU= 250, FRAMES=15):
-        """ Constructor for the Burning Ship set class which initializes the required parameters to defaults if none are provided.
+class BurningShip(Mandelbrot):  
+    def __init__(self, startX= -2, startY= -1.5, WIDTH= 3, HEIGHT= 3, DPU= 250, FRAMES=35):
+        """ Constructor for the Burning Ship fractal, which is a child of the Mandelbrot set.
+            This constructor will initialize the parameters to defaults if none are provided.
         
             :opt_param startX: x coordinate to start at on the x-axis
             :opt_param startY: y coordinate to start at on the y-axis
@@ -17,56 +19,21 @@ class BurningShip:
             :opt_param FRAMES: number of frames to generate in the gif
         """
 
-        self.start_x = startX
-        self.start_y = startY
-        self.width = WIDTH
-        self.height = HEIGHT
-        self.dpu = DPU
-        self.num_frames = FRAMES
-        self.real_axis = np.linspace(self.start_x, self.start_x + self.width, self.width * self.dpu)
-        self.imag_axis = np.linspace(self.start_y, self.start_y + self.height, self.height * self.dpu)
+        super().__init__(startX=startX, startY=startY, WIDTH=WIDTH, HEIGHT=HEIGHT, DPU=DPU, FRAMES=FRAMES)
 
     def __str__(self):
         return "Burning Ship set parameters;\n\tStart X: %d,\n\tStart Y: %d,\n\tWidth: %d,\n\tHeight: %d,\n\tDPU: %d,\n\tNUM FRAMES: %d" % (self.start_x, self.start_y, self.width, self.height, self.dpu, self.num_frames)
 
     def create_animation(self, OUTPUT_PATH=GIF_OUTPUT_PATH):
-        """ Creates a figSize_x by figSize_y figure, calls the animate function to plot the Burning Ship set on it, the save the resulting animation as a .gif file.
+        """ Passes the output path for the Burning SHip .gif to the parent's create_animation function in order to begin generating the animation.
             
             :opt_param OUTPATH_PATH: path to save the animated .gif of the Burning Ship set. Defaults to ./output/burningship.gif
         """
-        if not os.path.isfile(OUTPUT_PATH):
-            figSize_x = 10
-            figSize_y = 10
-            fig = plt.figure(figsize=(figSize_x, figSize_y))
+        super().create_animation(OUTPUT_PATH=OUTPUT_PATH)
 
-            anim = animation.FuncAnimation(fig, self.animate, frames=self.num_frames, interval=120, blit=True)
-            anim.save(OUTPUT_PATH, writer='ImageMagickWriter')
-    
-    def animate(self, i):
-        """ The animate function is called by matplotlib.animation librarys FuncAnimation to generate each frame in the output .gif file.
-            :param int i: the frame number, starting from 0, to animate.
-        """
-
-        ax = plt.axes() # axes obj
-        ax.clear()  # clear axes object
-        ax.set_xticks([], minor=False)   # clear x ticks
-        ax.set_yticks([], minor=False)   # clear y ticks
-
-        x = np.empty((len(self.real_axis), len(self.imag_axis)))    # re-initialize the array-like image
-        max_iterations = 256     # calculate the current threhold
-        num_iterations = round(1.15**(i+1)) 
-
-        for i in range(len(self.real_axis)):
-            for j in range(len(self.imag_axis)):
-                x[i, j] = self.burningship(self.real_axis[i], self.imag_axis[j], num_iterations)
-
-        # associate colours with iterations
-        img = ax.imshow(x.T, interpolation='bicubic', cmap='viridis')
-        return [img]
-
-    def burningship(self, x, y, num_iterations):
-        """ Calculates whether the number c (c = x + iy) belongs to the Mandelbrot set.
-            In order to belong to the Mandelbrot set, the sequence z[i+1] = z[i]**2 + c must NOT diverage after num_iterations.
+    def mandelbrot(self, x, y, num_iterations):
+        """ Calculates whether the number c (c = x + iy) belongs to the modified version of the Mandelbrot set known as the Burning Ship fractal.
+            In order to belong to the Burning Ship fractal, the sequence z[i+1] = (abs(Real z[i]) + abs(Imaginary z[i]))**2 + c must NOT diverage after num_iterations.
             The sequence diverges if the absolute value of z[i+1] > 4
 
             :param float x: x component of init complex #
@@ -79,9 +46,9 @@ class BurningShip:
         zy = y
         
         for i in range(num_iterations):
-            temp = zx**2 - zy**2 + x
+            next_zx = zx**2 - zy**2 + x
             zy = 2 * abs(zx*zy) + y
-            zx = temp
+            zx = next_zx
             z = complex(zx, zy)**2 + c
             if abs(z) > 4:
                return i
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fractals.mandelbrot import Mandelbrot
 from fractals.julia import Julia
 from fractals.burningship import BurningShip
 
-# m = Mandelbrot()
-# print(m)
-# m.create_animation()
+m = Mandelbrot()
+print(m)
+m.create_animation()
 
 j = Julia()
 print(j)
