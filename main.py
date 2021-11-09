cf88c85a761f764cfe87acd714d0a1d6daf8f99c
Matthew E. Grahlman <grahlmanmatthew@gmail.com>
2021-11-08 22:17:02 -0500

    Added newton fractal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fractals.mandelbrot import Mandelbrot
 from fractals.julia import Julia
 from fractals.burningship import BurningShip
+from fractals.newton import Newton
 
 m = Mandelbrot()
 print(m)
@@ -13,3 +14,7 @@ j.create_animation()
 b = BurningShip()
 print(b)
 b.create_animation()
+
+n = Newton(COLOUR_BY="iterations")
+print(n)
+n.create_animation()
